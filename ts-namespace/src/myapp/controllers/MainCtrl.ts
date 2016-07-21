bba9125fa64650cb15133365c591dad2f715e2be
Daniele Morosinotto <d.morosinotto@icloud.com>
2016-07-21 18:08:48 +0200

    Added BaseCtrl

--- ts-namespace/src/myapp/controllers/MainCtrl.ts
+++ ts-namespace/src/myapp/controllers/MainCtrl.ts
@@ -1,14 +1,26 @@
 namespace MyApp.Controllers {
-    export class MainCtrl {
-        static $inject = ["NameSvc"];
-        constructor(private svc: Services.NameSvc) {
+    class BaseCtrl { /* SAMPLE BASECLASS DON'T DO $inject HERE! */
+        public title: string;
+        constructor(public svc: Services.NameSvc) {
+            this.title = "Hello ";
+            svc.getName().then(t => this.title=t.value+" ");
+        }
+    }
+
+
+    export class MainCtrl extends BaseCtrl {
+        static $inject = ["NameSvc"]; // USE $inject HERE IN SUBCLASS AND PASS SVC TO super()
+        constructor(svc: Services.NameSvc) {
+            super(svc);
             this.name = 'World...';
-            svc.getName().then( n => this.name = n );
+            svc.getName().then( n => this.name = n.value );
         }
         
         public name: string
-        public show($event) {
-            window.alert($event);
+        public show($event: string | any) {
+            if (typeof $event === "string") {
+                if (window.confirm("Set " + $event + "?")) this.name = $event;
+            } else window.alert(JSON.stringify($event));
         }
     }
 }
